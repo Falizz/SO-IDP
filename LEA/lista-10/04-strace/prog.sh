1af4a357e6954779e76fc60b7d3f11c4e247e178
Falizz <fabioluisdecarvalhoterra@gmail.com>
2026-05-20 10:32:09 -0300

    LEA 10

---
+++ LEA/lista-10/04-strace/prog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /desafio
+
+strace -o /dev/null cat ./recompensa.txt
+
+# 99048CAD6DEE
---
+++ LEA/lista-10/05-as/prog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /desafio/
+
+as @/desafio/recompensa.txt
+
+# Assembler messages:
+# Fatal error: can't create a.out: Permission denied
